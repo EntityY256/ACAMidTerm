5bc02cef5912481094d808266d84897894025b63
EntityY256 <143442236+EntityY256@users.noreply.github.com>
2024-07-16 23:56:46 +0530

    REDO

--- Coin2.cpp
+++ Coin2.cpp
@@ -6,13 +6,12 @@ using namespace std;
 ll countWays(vector<int>& coins, int target_sum) {
     vector<ll> dp(target_sum + 1, 0);
     dp[0] = 1;
- 
-    // Iterate over each possible sum from 1 to target_sum
     for (ll coin : coins) {
-        // Update the dp array for each sum that can be achieved using the current coin
         for (ll i = coin; i <= target_sum; ++i) {
             dp[i] += dp[i - coin];
             dp[i] %= 1000000007;
+            // We consider coins greater than moving (increasing) values so that combinations gets counted ONLY ONCE
+            // Without this constraint, we can over count different permutations
         }
     }
     
@@ -20,11 +19,11 @@ ll countWays(vector<int>& coins, int target_sum) {
 }
  
 int main() {
-	int N,target_sum = 9;
+	int N,target_sum;
 	cin>>N>>target_sum;
 	vector<int> coins (N);
 	for(int i=0;i<N;i++) cin>>coins[i];
     sort(coins.begin(),coins.end());
     cout <<countWays(coins, target_sum) << endl;
     return 0;
-
+}
